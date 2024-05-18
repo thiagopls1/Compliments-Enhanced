017eaf63598ea1f6ebcd22a0ef34b8de7521bea6
Thiago Lins <thiagopls1@hotmail.com>
2024-05-18 16:56:24 -0300

    Change PORT from env connection conversion

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(parseInt(process.env.PORT));
+  await app.listen(process.env.PORT as unknown as number);
 }
 bootstrap();

5598f2fd4cef29bd456ba237f220039427c25687
Thiago Lins <thiagopls1@hotmail.com>
2024-06-05 21:43:46 -0300

    Add LoginComponent

---
+++ web/src/app/pages/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MaterialModule } from '../../../modules/material.module';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [MaterialModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+}
